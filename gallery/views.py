97dac618d48fc025b94bd3a5d138058b9833b2c6
Fridah-kalee <fridahkalee@gmail.com>
2022-05-27 12:48:13 +0300

    create photos view function

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
+def photos(request):
+    photo=Photos.objects.all()
+    return render(request,'photo.html',{"photo": photo}) 
