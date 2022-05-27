6bc6ecdf259137a8fa67d7d5b9a8f9933b666c53
Fridah-kalee <fridahkalee@gmail.com>
2022-05-27 13:31:39 +0300

    create models class

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
+class Photos(models.Model):
+    name = models.CharField(max_length=30)
+    photo_description = models.TextField()
+    photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
