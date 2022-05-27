db34e41da611250153342d9f53baf5331398a2c5
Fridah-kalee <fridahkalee@gmail.com>
2022-05-27 12:58:44 +0300

    create postgresql database

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,11 @@
 from django.http  import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request,"home.html")
 
-def photos(request):
-    photo=Photos.objects.all()
-    return render(request,'photo.html',{"photo": photo}) 
+# def photos(request):
+#     photo=Photos.objects.all()
+#     return render(request,'photo.html',{"photo": photo}) 
