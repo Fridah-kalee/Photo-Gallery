f25b69bd406b909986c6a98e816934204d71195e
Fridah-kalee <fridahkalee@gmail.com>
2022-05-29 19:22:54 +0300

    implement tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -21,6 +21,55 @@ class PhotosTestClass(TestCase):
         self.new_pet.save_photo()
         photo = Photos.objects.all()
 
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.Food= Category(category_name ='Food')
+        self.Food.save_category_name()
+
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Food, Category))
+
+    def test_save_category(self):
+        self.test_category = Category(category_name = 'food')
+        self.test_category.save_category_name()
+
+
+    def test_delete_category(self):
+        self.test_category = Category(category_name = 'food')
+        self.test_category.save_category_name()
+        self.test_category.delete_category_name()
+
+class LocationTestClass(TestCase):
+
+    # set up method
+
+    def setUp(self):
+        self.Kamakis = Location(location_name='Kamakis')
+        self.Kamakis.save_location_name()
+
+
+    def tearDown(self):
+        Location.objects.all().delete()
+ 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Kamakis, Location))
+
+    def test_save_method(self):
+        self.Kamakis.save_location_name()
+        Locations = Location.objects.all()
+        print(Locations)
+        self.assertTrue(len(Locations)==1)
+
+    def test_delete_method(self):
+        self.Kamakis.delete_location_name()
+        Locations = Location.objects.all()
+        print(Locations)
+        self.assertTrue(len(Locations)==0)               
+
 
 
 
