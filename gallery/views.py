e22a8dedc6c2a8452049c3fd0517ed00b1a8d8fe
Fridah-kalee <fridahkalee@gmail.com>
2022-05-29 15:00:29 +0300

    Adding for display

--- gallery/views.py
+++ gallery/views.py
@@ -2,9 +2,6 @@ from django.http  import HttpResponse
 from django.shortcuts import render,redirect
 from .models import Photos,Category
 
-# Create your views here.
-# def home(request):
-#     return render(request,"home.html")
 
 def home(request):
     photo = Photos.objects.all()
