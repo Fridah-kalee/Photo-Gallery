4a8fd3b9006ef6fb0a09d905a90fbe52b9a15856
Fridah-kalee <fridahkalee@gmail.com>
2022-05-29 18:22:45 +0300

    create tests and run them

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Photos,Category,Location
 
 # Create your tests here.
+class PhotosTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self. new_location = Location(location_name = 'Naivasha')
+        self. new_location.save()
+
+        self. new_category = Category(category_name = 'pet')
+        self. new_category.save()
+
+        self. new_pet = Photos(name = 'cat', photo_description = 'cute', photo_location = self.new_location, photo_category = self.new_category)
+        self. new_pet.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pet, Photos))
+
+    def test_save_pet(self):
+        self.new_pet.save_photo()
+        photo = Photos.objects.all()
+
+
+
+
+      
+
