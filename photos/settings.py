af1750bc236555248228d0012d02f8d2055041a1
Fridah-kalee <fridahkalee@gmail.com>
2022-05-30 17:38:56 +0300

    deployment

--- photos/settings.py
+++ photos/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import django_heroku
+import django_on_heroku
 import dj_database_url
 from decouple import config,Csv
 from pathlib import Path
@@ -141,4 +141,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
