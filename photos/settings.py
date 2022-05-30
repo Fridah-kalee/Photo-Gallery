ef1453919bab0860285b56fb1a51bdc347090c2b
Fridah-kalee <fridahkalee@gmail.com>
2022-05-30 18:02:08 +0300

    deployment

--- photos/settings.py
+++ photos/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
+
+
 
 ALLOWED_HOSTS = []
 
